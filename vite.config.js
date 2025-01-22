fbaa6e7efaa302c0c6ae24b4e9c3910ca43e1d09
roronovazoro2 <monkeydluffy_joy@proton.me>
2025-01-22 19:10:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
+// Replace 'your-repo-name' with the actual GitHub repository name
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: '/viteapp/', 
 });
